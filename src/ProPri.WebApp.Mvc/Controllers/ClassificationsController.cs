e3c9b0274bd81c706c253dae3615b5c8ac516b67
Victor Fernandes <victor.m.fernandes@outlook.com>
2019-12-03 17:04:28 -0300

    Tree view

---
+++ src/ProPri.WebApp.Mvc/Controllers/ClassificationsController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProPri.WebApp.Mvc.Views.Palavras.ViewModels;
+using System;
+
+namespace ProPri.WebApp.Mvc.Controllers
+{
+    public class ClassificationsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        public IActionResult Edit()
+        {
+            return View();
+        }
+
+        public IActionResult Delete(Guid id)
+        {
+            // Pegar palavra
+            // Checar se é null
+            return PartialView("_Delete", new PalavraIndexViewModel { Id = id });
+        }
+
+        [HttpPost]
+        public IActionResult Delete(PalavraIndexViewModel palavraIndexVm)
+        {
+            var url = Url.Action("Index", "Palavras");
+            return Json(new { success = true, url });
+        }
+    }
+}
