146d841b1c85df132271c6d6b505df86916d87a6
Victor Fernandes <victor.m.fernandes@outlook.com>
2019-12-03 08:37:06 -0300

    Criando ações básicas de palavras, melhorando a tabela de index

---
+++ src/ProPri.WebApp.Mvc/Views/Palavras/ViewModels/PalavraFormViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProPri.WebApp.Mvc.Views.Palavras.ViewModels
+{
+    public class PalavraFormViewModel
+    {
+        public Guid Id { get; set; }
+        public string TextoPortugues { get; set; }
+        public string TextoIngles { get; set; }
+        public string ImagemUpload { get; set; }
+    }
+}
---
+++ src/ProPri.WebApp.Mvc/Views/Palavras/ViewModels/PalavraIndexViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace ProPri.WebApp.Mvc.Views.Palavras.ViewModels
+{
+    public class PalavraIndexViewModel
+    {
+        public Guid Id { get; set; }
+        [DisplayName("Texto Português")]
+        public string TextoPortugues { get; set; }
+        public string TextoIngles { get; set; }
+    }
+}
